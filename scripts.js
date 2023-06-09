d388b21efe5fc8bf3dd5d6f5d59d46fccdebc9ec
ShanaySharif <Shanay.uw17@gmail.com>
2023-06-09 12:17:36 -0400

    will add a function to scripts and a body to the HTML

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function generateList() {
+    let userInput = parseInt(document.getElementById("user-input").value);
+    let resultList = document.getElementById("result-list");
+
+    let list = [];
+    for (let i = 0; i <= userInput; i++) {
+    
