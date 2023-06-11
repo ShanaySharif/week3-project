889c17a4615bb79562b0ce6f7aed336e2d7aeef6
ShanaySharif <Shanay.uw17@gmail.com>
2023-06-11 13:32:26 -0400

    will add branching logic.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,25 @@
+//Business Logic 
+
 function testGenerateListFunction(userInput) {
     let list = [];
     for (let i = 0; i <= userInput; i++) {
       let value = i.toString();
+      if (value.includes("3")) {
+        list.push("Won't you be my neighbor?");
+      } else if (value.includes("2")) {
+        list.push("Boop!");
+      } else if (value.includes("1")) {
+        list.push("Beep!");
+      } else {
+        list.push(value);
+      }
+    }
+
+
+
+
+
+
+
+
+      //UI
